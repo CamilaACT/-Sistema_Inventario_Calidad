45c364234a12eaf9e28610a55dde2cfdf5337879
TommysNJ <130273419+TommysNJ@users.noreply.github.com>
2025-07-05 14:53:29 +0000

    Inicializa proyecto Java con HTML

--- src/main/java/sistema/inventario/Main.java
+++ src/main/java/sistema/inventario/Main.java
@@ -10,9 +10,14 @@ public class Main {
      * @param args argumentos
      */
     public static void main(String[] args) {
+        IOutput htmlOutput = new HtmlOutput("output/index.html");
+        Inventory htmlInventory = new Inventory(htmlOutput);
+        htmlInventory.addProduct("Monitor", 5, 129.99);
+        htmlInventory.addProduct("Teclado", 10, 39.99);
+        htmlInventory.printInventory();
+
         IOutput output = new ConsoleOutput();
         Inventory inventory = new Inventory(output);
-
         inventory.addProduct("Monitor", 5, 129.99);
         inventory.addProduct("Teclado", 10, 39.99);
         inventory.printInventory();
